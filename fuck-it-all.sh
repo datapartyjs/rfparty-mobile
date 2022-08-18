28b96df8cee6ed50935e70ce844dfec625da4baf
Alan Meekins <alanm@roshub.io>
2022-08-18 07:06:05 -0700

    building on api32

--- fuck-it-all.sh
+++ fuck-it-all.sh
@@ -5,9 +5,9 @@ cordova clean android
 cordova platform rm android
 rm -rf node_modules
 npm i
-cordova platform add android@9
+cordova platform add android@latest
 #cordova plugin add cordova-plugin-androidx
 cordova plugin add nodejs-mobile-cordova
 cordova plugin add cordova-plugin-exclude-files
 cordova build android --debug
-ls -la platforms/android/app/build/outputs/apk/debug/app-debug.apk
+ls -lah platforms/android/app/build/outputs/apk/debug/app-debug.apk
